77e4f6093dfa3d861e06df77687c62d7b59a75f8
znb <znb20010119@163.com>
2021-12-07 21:41:38 +0800

    实现活动

---
+++ src/main/java/com/logistics_management/dao/ActivityDao.java
@@ -0,0 +1,8 @@
+package com.logistics_management.dao;
+
+import com.logistics_management.domain.Activity;
+import tk.mybatis.mapper.common.Mapper;
+
+
+public interface ActivityDao extends Mapper<Activity> {
+}
