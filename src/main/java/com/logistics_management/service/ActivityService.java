a843e0b93cb7a5135f55b167f2017b67318c6eeb
znb <znb20010119@163.com>
2022-02-05 18:21:18 +0800

    2022年2月5日 18:20:54

--- src/main/java/com/logistics_management/service/ActivityService.java
+++ src/main/java/com/logistics_management/service/ActivityService.java
@@ -8,17 +8,52 @@ import java.util.Map;
 
 public interface ActivityService {
 
+    /**
+     * 查询所以活动
+     * @return
+     */
     List<Activity> findAll();
 
+    /**
+     * 分页查询
+     * @param searchMap
+     * @return
+     */
     Page<Activity> search(Map searchMap);
 
+    /**
+     * 添加活动
+     * @param activity
+     * @return
+     */
     Boolean addActivity(Activity activity);
 
+    /**
+     * 根据id查询活动
+     * @param id
+     * @return
+     */
     Activity findActivityById(Integer id);
 
+    /**
+     * 更新活动
+     * @param activity
+     * @return
+     */
     Boolean update(Activity activity);
 
+    /**
+     * 删除活动
+     * @param ids
+     * @return
+     */
     Boolean del(List<Integer> ids);
 
+    /**
+     * 更新活动信息状态码
+     * @param status
+     * @param id
+     * @return
+     */
     Boolean updateStatus(String status, Integer id);
 }
